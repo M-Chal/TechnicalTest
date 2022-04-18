99b679e15992f35a997c27d52a784519350b084a
Mateusz Chalubowicz <45897333+M-Chal@users.noreply.github.com>
2022-04-18 22:10:12 +0100

    temporarily commented api call to deploy

--- pages/phone.js
+++ pages/phone.js
@@ -4,26 +4,26 @@ import styles from '../styles/Home.module.css'
 import Sidebar from '../components/Sidebar'
 import PhoneLog from '../components/PhoneLog'
 
-export const getStaticProps = async () => {
-  // For some reason using the usual "/api/getCallLog" didn't work here
-  // So I had to add the full path, We'll see if it works once deployed
-  const response = await fetch("http://localhost:3000/api/getCallLog",{
-    method:"GET",
-    headers:{
-        "Content-Type": "applications/json",
-    },
-  })
-  const data = await response.json()
-  return {
-    props: {log: data} 
-  }
-}
+// export const getStaticProps = async () => {
+//   // For some reason using the usual "/api/getCallLog" didn't work here
+//   // So I had to add the full path, We'll see if it works once deployed
+//   const response = await fetch("/api/calls",{
+//     method:"GET",
+//     headers:{
+//         "Content-Type": "applications/json",
+//     },
+//   })
+//   const data = await response.json()
+//   return {
+//     props: {log: data} 
+//   }
+// }
 
 function phone({log}) {
   return (
     <div className={styles.main}>
       <Sidebar />
-      <PhoneLog log={log}/>
+      {/* <PhoneLog log={log}/> */}
     </div>
   )
 }
